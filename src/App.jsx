e16ccbf5f7778bd69f85c61eea4ba5d16fade3e6
HuynhAnh24h <anhdev24h@gmail.com>
2025-05-13 08:53:53 +0700

    Create Front end

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-900'>Huỳnh Anh Deveeloper Blog</h1>
+    </div>
+  )
+}
+
+export default App
